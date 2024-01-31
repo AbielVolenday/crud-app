d9e2fd9b678053ca05a678b4b88b405e8e5c866e
Abiel <abiel.rodriguez@volenday.com>
2024-01-31 09:17:59 +0000

    installing utility libraries  (shadcn , react-query , lucide-react , date-fns , vercel-postgress)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,10 @@
+/* eslint-disable  */
+
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Providers } from "@/lib/useQueryProvider";
+import { Header } from "@/components/header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +20,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <Providers>
+        <body className={inter.className}>
+                  <Header></Header>
+                  {children}
+        </body>
+      </Providers>
     </html>
   );
 }
